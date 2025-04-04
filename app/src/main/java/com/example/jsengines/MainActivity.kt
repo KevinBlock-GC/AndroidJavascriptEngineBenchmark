bfcdab53165b573229ceb79930a635c625a26099
Kevin Block <kevin.block@gc.com>
2025-04-04 18:43:40 -0400

    Add option to execute all engines

--- app/src/main/java/com/example/jsengines/MainActivity.kt
+++ app/src/main/java/com/example/jsengines/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jsengines
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,19 +24,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.jsengines.engines.DuktapeEngineImpl
 import com.example.jsengines.engines.J2V8EngineImpl
 import com.example.jsengines.engines.JavascriptSandboxImpl
 import com.example.jsengines.engines.JavetEngineImpl
 import com.example.jsengines.engines.QuickJSEngineImpl
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 import kotlin.system.measureTimeMillis
 
-private val runtimes = mutableMapOf<String, MutableList<Long>>()
 private val benchmarkScript =
     """
         function createLargeObject(depth, breadth) {
@@ -48,38 +51,43 @@ private val benchmarkScript =
             }
             return obj;
         }
-        JSON.stringify(createLargeObject(5, 5));
+        JSON.stringify(createLargeObject(3, 5));
         """.trimIndent()
 
-private fun runEngineTest(engine: JSEngine): Long {
-    engine.init()
-    val runtime = measureTimeMillis {
-        for (i in 0..100) {
-            engine.evaluate(benchmarkScript)
-        }
-    }
-    engine.close()
-    val engineName = engine::class.java.simpleName
-    if (!runtimes.containsKey(engineName)) {
-        runtimes[engineName] = mutableListOf(runtime)
-    } else {
-        runtimes[engineName]!!.add(runtime)
-    }
-    return runtime
+private enum class Engine {
+    J2V8,
+    JAVET,
+    QUICKJS,
+    JAVASCRIPTSANDBOX,
+    DUKTAPE
 }
 
+private val engines: Map<Engine, (Context) -> JSEngine> = mapOf(
+    Engine.J2V8 to { J2V8EngineImpl() },
+    Engine.JAVET to { JavetEngineImpl() },
+    Engine.QUICKJS to { QuickJSEngineImpl() },
+    Engine.JAVASCRIPTSANDBOX to { JavascriptSandboxImpl(it) },
+    Engine.DUKTAPE to { DuktapeEngineImpl() }
+)
+
 class MainActivity : ComponentActivity() {
+
+    private val runtimes = MutableStateFlow<Map<String, List<Long>>>(emptyMap())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             var runtime by remember { mutableLongStateOf(0L) }
             var isEnabled: Boolean by remember { mutableStateOf(true) }
             val coroutineScope = rememberCoroutineScope()
+            val context = this
+
+            val averageRuntimes by runtimes.collectAsStateWithLifecycle()
 
             @Composable
             fun BenchmarkButton(
                 text: String,
-                engine: () -> JSEngine
+                engine: Engine
             ) {
                 Button(
                     content = { Text(text = text) },
@@ -87,7 +95,8 @@ class MainActivity : ComponentActivity() {
                         if (!isEnabled) return@Button
                         coroutineScope.launch(Dispatchers.IO) {
                             isEnabled = false
-                            runtime = runEngineTest(engine())
+                            val jsEngine = engines[engine]!!(context)
+                            runtime = runEngineTest(jsEngine)
                             isEnabled = true
                         }
                     }
@@ -102,23 +111,31 @@ class MainActivity : ComponentActivity() {
             ) {
                 BenchmarkButton(
                     text = "Run J2V8",
-                    engine = { J2V8EngineImpl() }
+                    engine = Engine.J2V8
                 )
                 BenchmarkButton(
                     text = "Run Javet",
-                    engine = { JavetEngineImpl() }
+                    engine = Engine.JAVET
                 )
                 BenchmarkButton(
                     text = "Run QuickJS",
-                    engine = { QuickJSEngineImpl() }
+                    engine = Engine.QUICKJS
                 )
                 BenchmarkButton(
                     text = "Run JavascriptSandbox",
-                    engine = { JavascriptSandboxImpl(this@MainActivity) }
+                    engine = Engine.JAVASCRIPTSANDBOX
                 )
                 BenchmarkButton(
                     text = "Run Duktape",
-                    engine = { DuktapeEngineImpl() }
+                    engine = Engine.DUKTAPE
+                )
+                Button(
+                    content = {
+                        Text(text = "Run All Engines")
+                    },
+                    onClick = {
+                        runAllEngines(coroutineScope)
+                    }
                 )
                 Spacer(modifier = Modifier.height(50.dp))
 
@@ -134,9 +151,9 @@ class MainActivity : ComponentActivity() {
                     )
                 }
 
-                if (runtimes.isNotEmpty()) {
+                if (averageRuntimes.isNotEmpty()) {
                     Text(text = "Runtimes:")
-                    runtimes.forEach { (engine, runtimes) ->
+                    averageRuntimes.forEach { (engine, runtimes) ->
                         val avgRuntime = runtimes.average().roundToInt()
                         Text(text = "$engine (${runtimes.size}) Avg Runtime: $avgRuntime ms")
                     }
@@ -144,18 +161,29 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun runEngineTest(engine: JSEngine): Long {
+        engine.init()
+        val runtime = measureTimeMillis {
+            for (i in 0..25) {
+                engine.evaluate(benchmarkScript)
+            }
+        }
+        engine.close()
+        val engineName = engine::class.java.simpleName
+        runtimes.update {
+            val currentRuntimes = it[engineName] ?: emptyList()
+            it + (engineName to (currentRuntimes + runtime))
+        }
+        return runtime
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("Android")
+    private fun runAllEngines(scope: CoroutineScope) {
+        engines.forEach { (_, engineFactory) ->
+            scope.launch(Dispatchers.IO) {
+                val jsEngine = engineFactory(this@MainActivity)
+                runEngineTest(jsEngine)
+            }
+        }
+    }
 }
